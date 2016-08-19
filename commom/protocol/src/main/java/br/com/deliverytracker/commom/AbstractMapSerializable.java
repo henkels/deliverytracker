a602493aea6f3f28951723fdc9d66396cdf083b6
Tarcisio Henkels <tarcisio.henkels@gmail.com>
2016-08-18 23:44:43 -0300

    PUSH SERV|ICE

--- commom/protocol/src/main/java/br/com/deliverytracker/commom/AbstractMapSerializable.java
+++ commom/protocol/src/main/java/br/com/deliverytracker/commom/AbstractMapSerializable.java
@@ -39,7 +39,40 @@ public abstract class AbstractMapSerializable {
 	}
 	
 	
-	void unserializeFrom(LinkedHashMap<String, String> data, String parentContext);
+	void unserializeFrom(LinkedHashMap<String, String> data, String parentContext){
+		StringBuilder ctx = new StringBuilder(parentContext);
+		int len = ctx.length();
+		Field[] fields = this.getClass().getFields();
+		for (Field field : fields) {
+			ctx.setLength(len);
+			ctx.append(field.getName());
+			try {
+				Class<?> clazz = field.getType();
+				Object obj = field.get(this);
+				if (obj != null) {
+					if (obj instanceof AbstractMapSerializable) {
+						ctx.append('.');
+						// É uma derivação ou implementação
+						if (field.getType() != obj.getClass()) {
+							// Salve a classe em questão
+							int newLen = ctx.length();
+							ctx.append("class");
+							data.put(ctx.toString(), obj.getClass().getSimpleName());
+							ctx.setLength(newLen);
+						}
+						AbstractMapSerializable serializable = (AbstractMapSerializable) obj;
+						ctx.append('.');
+						serializable.serializeTo(data, ctx.toString());
+					} else {
+						data.put(ctx.toString(), obj.toString());
+					}
+				}
+			} catch (Exception e) {
+				// TODO: handle exception
+			}
+		}
+		
+	}
 	
 	
 
