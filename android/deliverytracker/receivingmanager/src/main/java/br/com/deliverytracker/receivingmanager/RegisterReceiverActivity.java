28d919360bc5342b622a45e848ce2d47ee071555
Tarcisio Henkels <tarcisio.henkels@gmail.com>
2016-11-12 17:01:20 -0200

    impl

--- android/deliverytracker/receivingmanager/src/main/java/br/com/deliverytracker/receivingmanager/RegisterReceiverActivity.java
+++ android/deliverytracker/receivingmanager/src/main/java/br/com/deliverytracker/receivingmanager/RegisterReceiverActivity.java
@@ -2,6 +2,7 @@ package br.com.deliverytracker.receivingmanager;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class RegisterReceiverActivity extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class RegisterReceiverActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register_receiver);
+        ListView adresses_LV = (ListView) findViewById(R.id.adresses_LV);
     }
 }
